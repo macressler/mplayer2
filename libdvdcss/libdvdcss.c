c127d44e82d79142c97f7b16d9f6809a3d42e6c8
Josh <redxii@smplayer2.fake>
2012-12-31 15:57:44 -0500

    libdvdcss: disable path code
    This causes a crash on DVDs when using LOCAL_APPDATA_FONTCONFIG_CACHE as
    the cachedir and ASS is enabled. Some GUIs always enable ASS regardless if
    an ASS subtitle is being used or not.

--- libdvdcss/libdvdcss.c
+++ libdvdcss/libdvdcss.c
@@ -238,50 +238,50 @@ LIBDVDCSS_EXPORT dvdcss_t dvdcss_open ( char *psz_target )
      */
     if( psz_cache == NULL || psz_cache[0] == '\0' )
     {
-#if HAVE_DIRECT_H
-        typedef HRESULT( WINAPI *SHGETFOLDERPATH )
-                       ( HWND, int, HANDLE, DWORD, LPTSTR );
+// #if HAVE_DIRECT_H
+        // typedef HRESULT( WINAPI *SHGETFOLDERPATH )
+                       // ( HWND, int, HANDLE, DWORD, LPTSTR );
 
-#   define CSIDL_FLAG_CREATE 0x8000
-#   define CSIDL_APPDATA 0x1A
-#   define SHGFP_TYPE_CURRENT 0
+// #   define CSIDL_FLAG_CREATE 0x8000
+// #   define CSIDL_APPDATA 0x1A
+// #   define SHGFP_TYPE_CURRENT 0
 
-        char psz_home[MAX_PATH];
-        HINSTANCE p_dll;
-        SHGETFOLDERPATH p_getpath;
+        // char psz_home[MAX_PATH];
+        // HINSTANCE p_dll;
+        // SHGETFOLDERPATH p_getpath;
 
-        *psz_home = '\0';
+        // *psz_home = '\0';
 
-        /* Load the shfolder dll to retrieve SHGetFolderPath */
-        p_dll = LoadLibrary( "shfolder.dll" );
-        if( p_dll )
-        {
-            p_getpath = (void*)GetProcAddress( p_dll, "SHGetFolderPathA" );
-            if( p_getpath )
-            {
-                /* Get the "Application Data" folder for the current user */
-                if( p_getpath( NULL, CSIDL_APPDATA | CSIDL_FLAG_CREATE,
-                               NULL, SHGFP_TYPE_CURRENT, psz_home ) == S_OK )
-                {
-                    FreeLibrary( p_dll );
-                }
-                else
-                {
-                    *psz_home = '\0';
-                }
-            }
-            FreeLibrary( p_dll );
-        }
+        // /* Load the shfolder dll to retrieve SHGetFolderPath */
+        // p_dll = LoadLibrary( "shfolder.dll" );
+        // if( p_dll )
+        // {
+            // p_getpath = (void*)GetProcAddress( p_dll, "SHGetFolderPathA" );
+            // if( p_getpath )
+            // {
+                // /* Get the "Application Data" folder for the current user */
+                // if( p_getpath( NULL, CSIDL_APPDATA | CSIDL_FLAG_CREATE,
+                               // NULL, SHGFP_TYPE_CURRENT, psz_home ) == S_OK )
+                // {
+                    // FreeLibrary( p_dll );
+                // }
+                // else
+                // {
+                    // *psz_home = '\0';
+                // }
+            // }
+            // FreeLibrary( p_dll );
+        // }
 
-        /* Cache our keys in
-         * C:\Documents and Settings\$USER\Application Data\dvdcss\ */
-        if( *psz_home )
-        {
-            snprintf( psz_buffer, PATH_MAX, "%s/dvdcss", psz_home );
-            psz_buffer[PATH_MAX-1] = '\0';
-            psz_cache = psz_buffer;
-        }
-#else
+        // /* Cache our keys in
+         // * C:\Documents and Settings\$USER\Application Data\dvdcss\ */
+        // if( *psz_home )
+        // {
+            // snprintf( psz_buffer, PATH_MAX, "%s/dvdcss", psz_home );
+            // psz_buffer[PATH_MAX-1] = '\0';
+            // psz_cache = psz_buffer;
+        // }
+// #else
         char *psz_home = NULL;
 #   ifdef HAVE_PWD_H
         struct passwd *p_pwd;
@@ -328,7 +328,7 @@ LIBDVDCSS_EXPORT dvdcss_t dvdcss_open ( char *psz_target )
             psz_buffer[PATH_MAX-1] = '\0';
             psz_cache = psz_buffer;
         }
-#endif
+//#endif
     }
 
     /*
