8f0ea2080077e0f6aaa2892b725af3608cd0eef5
Josh <redxii@smplayer2.fake>
2013-01-21 17:23:38 -0500

    Set Libav version to unknown if snapshot_libav doesn't exist

--- version.sh
+++ version.sh
@@ -13,11 +13,14 @@ test $git_revision || git_revision=UNKNOWN
 version=$(cat VERSION 2> /dev/null)
 test $version || version=$git_revision
 
+libav_version=$(cat snapshot_libav 2> /dev/null)
+test $libav_version || libav_version=UNKNOWN
+
 NEW_REVISION="#define VERSION \"${version}${extra}\""
 OLD_REVISION=$(head -n 1 version.h 2> /dev/null)
 BUILD_DATE="#define BUILD_DATE \"$(date)\""
-LIBAV_VERSION="#define LIBAV_VERSION \"$(cat snapshot_libav 2> /dev/null)\""
-TITLE='#define MP_TITLE "%s "VERSION" (C) 2000-2013 MPlayer2 Team\nCustom build by Redxii, http://smplayer.sourceforge.net\nBuild date: "BUILD_DATE"\nCompiled against Libav version "LIBAV_VERSION"\n\n"'
+LIBAV_VERSION="#define LIBAV_VERSION \"${libav_version}\""
+TITLE='#define MP_TITLE "%s "VERSION" (C) 2000-2013 MPlayer2 Team\nCustom build by Redxii, http://smplayer.sourceforge.net\nCompiled against Libav version "LIBAV_VERSION"\nBuild date: "BUILD_DATE"\n\n"'
 
 # Update version.h only on revision changes to avoid spurious rebuilds
 if test "$NEW_REVISION" != "$OLD_REVISION"; then
